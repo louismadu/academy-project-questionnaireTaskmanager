6077ed8cbd9283726a41bf977694cf0ef0702406
Chukwuebuka Madu <louisfrancismaduc@gmail.com>
2024-10-17 10:44:47 -0400

    ReadMe

---
+++ src/main/java/academy_project/questionaire_task_manager/App.java
@@ -0,0 +1,13 @@
+package academy_project.questionaire_task_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
